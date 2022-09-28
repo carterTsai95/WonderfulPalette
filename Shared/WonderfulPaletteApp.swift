e0ca0f19295a009f1ed66599418f066f0efc8fd0
Hung-Chun(Carter) Tsai <tsaihong1995@gmail.com>
2022-09-27 21:18:34 -0400

    Initial Commit

---
+++ Shared/WonderfulPaletteApp.swift
@@ -0,0 +1,17 @@
+//
+//  WonderfulPaletteApp.swift
+//  Shared
+//
+//  Created by Hung-Chun Tsai on 2022-09-27.
+//
+
+import SwiftUI
+
+@main
+struct WonderfulPaletteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
