26fc4ec266c7fe95a981d7c71a29c24adeaa1baa
Hung-Chun(Carter) Tsai <tsaihong1995@gmail.com>
2022-09-29 18:57:21 -0400

    Adding example view for the colour palette

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -8,9 +8,131 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var selectedColor: Color = .red
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            ColorPicker(selection: $selectedColor) {
+                Text("Select Primary Color")
+            }.padding()
+            VStack {
+                Text("Primary Color")
+                    .fontWeight(.bold)
+                HStack {
+                    Spacer()
+                    Circle()
+                        .foregroundColor(selectedColor)
+                        .frame(width: 45, height: 45)
+                    Spacer()
+                }
+                
+                //Complement
+                VStack(alignment: .leading) {
+                    HStack {
+                        Text("1. Complement")
+                            .fontWeight(.medium)
+                        Spacer()
+                    }
+                    Divider()
+                    HStack {
+                        Rectangle()
+                            .foregroundColor(selectedColor)
+                            .frame(width: 50, height: 50)
+                        Rectangle()
+                            .foregroundColor(selectedColor.complement)
+                            .frame(width: 50, height: 50)
+                    }
+                    
+                }.padding([.horizontal, .bottom], 10)
+                
+                //Split Complement
+                VStack(alignment: .leading) {
+                    HStack {
+                        Text("2. Split Complement")
+                            .fontWeight(.medium)
+                        Spacer()
+                    }
+                    Divider()
+                    HStack {
+                        Rectangle()
+                            .foregroundColor(selectedColor)
+                            .frame(width: 50, height: 50)
+                        Rectangle()
+                            .foregroundColor(selectedColor.splitComplement0)
+                            .frame(width: 50, height: 50)
+                        Rectangle()
+                            .foregroundColor(selectedColor.splitComplement1)
+                            .frame(width: 50, height: 50)
+                    }
+                }.padding([.horizontal, .bottom], 10)
+
+                //Triadic
+                VStack(alignment: .leading) {
+                    HStack {
+                        Text("3. Triadic")
+                            .fontWeight(.medium)
+                        Spacer()
+                    }
+                    Divider()
+                    HStack {
+                        Rectangle()
+                            .foregroundColor(selectedColor)
+                            .frame(width: 50, height: 50)
+                        Rectangle()
+                            .foregroundColor(selectedColor.triadic0)
+                            .frame(width: 50, height: 50)
+                        Rectangle()
+                            .foregroundColor(selectedColor.triadic1)
+                            .frame(width: 50, height: 50)
+                    }
+                }.padding([.horizontal, .bottom], 10)
+
+                //Tetradic
+                VStack(alignment: .leading) {
+                    HStack {
+                        Text("4. Tetradic")
+                            .fontWeight(.medium)
+                        Spacer()
+                    }
+                    Divider()
+                    HStack {
+                        Rectangle()
+                            .foregroundColor(selectedColor)
+                            .frame(width: 50, height: 50)
+                        Rectangle()
+                            .foregroundColor(selectedColor.tetradic0)
+                            .frame(width: 50, height: 50)
+                        Rectangle()
+                            .foregroundColor(selectedColor.tetradic1)
+                            .frame(width: 50, height: 50)
+                        Rectangle()
+                            .foregroundColor(selectedColor.tetradic2)
+                            .frame(width: 50, height: 50)
+                    }
+                }.padding([.horizontal, .bottom], 10)
+
+                //Analagous
+                VStack(alignment: .leading) {
+                    HStack {
+                        Text("5. Tetradic")
+                            .fontWeight(.medium)
+                        Spacer()
+                    }
+                    Divider()
+                    HStack {
+                        Rectangle()
+                            .foregroundColor(selectedColor)
+                            .frame(width: 50, height: 50)
+                        Rectangle()
+                            .foregroundColor(selectedColor.analagous0)
+                            .frame(width: 50, height: 50)
+                        Rectangle()
+                            .foregroundColor(selectedColor.analagous1)
+                            .frame(width: 50, height: 50)
+                    }
+                }.padding([.horizontal, .bottom], 10)
+            }
+            Spacer()
+        }
     }
 }
 
