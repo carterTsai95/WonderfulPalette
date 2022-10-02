b1a16c0f8c05b33484087fe0978ba3a2e04ccd96
Hung-Chun(Carter) Tsai <tsaihong1995@gmail.com>
2022-10-02 11:54:44 -0400

    Adding Preview Helper to show each text size and dard mode's view

---
+++ Shared/View/Components/ColorPatternContainer.swift
@@ -0,0 +1,20 @@
+//
+//  ColorPatternContainer.swift
+//  WonderfulPalette (iOS)
+//
+//  Created by Hung-Chun Tsai on 2022-10-02.
+//
+
+import SwiftUI
+
+struct ColorPatternContainer: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ColorPatternContainer_Previews: PreviewProvider {
+    static var previews: some View {
+        ColorPatternContainer()
+    }
+}
