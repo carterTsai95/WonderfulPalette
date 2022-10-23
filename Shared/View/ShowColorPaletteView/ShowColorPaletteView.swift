80e3cb16334881fb388016c013bffa5b419a9b9b
Hung-Chun(Carter) Tsai <tsaihong1995@gmail.com>
2022-10-22 20:59:00 -0400

    Adding the TabView. Extracting the ColorPaletteView

---
+++ Shared/View/ShowColorPaletteView/ShowColorPaletteView.swift
@@ -0,0 +1,93 @@
+//
+//  ShowColorPaletteView.swift
+//  WonderfulPalette
+//
+//  Created by Hung-Chun Tsai on 2022-10-22.
+//
+
+import SwiftUI
+
+struct ShowColorPaletteView: View {
+    @State private var selectedColor: Color = .red
+
+    var body: some View {
+        ScrollView {
+            ColorPicker(selection: $selectedColor) {
+                Text("Select Primary Color")
+            }.padding()
+            VStack {
+                Text("Primary Color")
+                    .fontWeight(.bold)
+                HStack {
+                    Spacer()
+                    Circle()
+                        .foregroundColor(selectedColor)
+                        .frame(width: 45, height: 45)
+                    Spacer()
+                }
+
+                // Complement
+                ColorPatternContainer(
+                    headerTitle: "1. Complement"
+                ) {
+                    HStack(spacing: 1.5) {
+                        ColorCellView(color: selectedColor)
+                        ColorCellView(color: selectedColor.complement)
+                    }
+                }
+
+                // Split Complement
+                ColorPatternContainer(
+                    headerTitle: "2. Split Complement"
+                ) {
+                    HStack(spacing: 1.5) {
+                        ColorCellView(color: selectedColor)
+                        ColorCellView(color: selectedColor.splitComplement0)
+                        ColorCellView(color: selectedColor.splitComplement1)
+                    }
+                }
+
+                // Triadic
+                ColorPatternContainer(
+                    headerTitle: "3. Triadic"
+                ) {
+                    HStack(spacing: 1.5) {
+                        ColorCellView(color: selectedColor)
+                        ColorCellView(color: selectedColor.triadic0)
+                        ColorCellView(color: selectedColor.triadic1)
+                    }
+                }
+
+                // Tetradic
+                ColorPatternContainer(
+                    headerTitle: "4. Tetradic"
+                ) {
+                    HStack(spacing: 1.5) {
+                        ColorCellView(color: selectedColor)
+                        ColorCellView(color: selectedColor.tetradic0)
+                        ColorCellView(color: selectedColor.tetradic1)
+                        ColorCellView(color: selectedColor.tetradic2)
+                    }
+                }
+
+                // Analagous
+                ColorPatternContainer(
+                    headerTitle: "5. Analagous"
+                ) {
+                    HStack(spacing: 1.5) {
+                        ColorCellView(color: selectedColor)
+                        ColorCellView(color: selectedColor.analagous0)
+                        ColorCellView(color: selectedColor.analagous1)
+                    }
+                }
+            }
+            Spacer()
+        }
+    }
+}
+
+struct ShowColorPaletteView_Previews: PreviewProvider {
+    static var previews: some View {
+        ShowColorPaletteView()
+    }
+}
